f98e16a7ff4bd9ef91ba1e74749c3b49dd10aa40
Tony Josi <tonyjosi@amazon.com>
2023-07-25 08:53:50 +0530

    initial

---
+++ src/Renesas_RX72N_Envision_Kit_CC_FreeRTOS_TCP.c
@@ -0,0 +1,17 @@
+/***********************************************************************
+*
+*  FILE        : Renesas_RX72N_Envision_Kit_CC_FreeRTOS_TCP.c
+*  DATE        : 2023-07-25
+*  DESCRIPTION : Main Program
+*
+*  NOTE:THIS IS A TYPICAL EXAMPLE.
+*
+***********************************************************************/
+#include "r_smc_entry.h"
+
+void main(void);
+
+void main(void)
+{
+
+}
