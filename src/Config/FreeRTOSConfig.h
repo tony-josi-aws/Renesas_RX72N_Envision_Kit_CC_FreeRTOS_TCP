b3773f8da03b89e9a2b9ece1a0bc6599afda2cb8
Tony Josi <tonyjosi@amazon.com>
2023-07-27 00:13:49 +0530

    increase heap

--- src/Config/FreeRTOSConfig.h
+++ src/Config/FreeRTOSConfig.h
@@ -51,7 +51,7 @@
 #define configPERIPHERAL_CLOCK_HZ		(BSP_PCLKB_HZ)
 #define configTICK_RATE_HZ				(( TickType_t ) 1000)
 #define configMINIMAL_STACK_SIZE		(( unsigned short ) 140)
-#define configTOTAL_HEAP_SIZE_N			(192)
+#define configTOTAL_HEAP_SIZE_N			(256)
 #define configTOTAL_HEAP_SIZE			(( size_t ) ( configTOTAL_HEAP_SIZE_N * 1024 ))
 #define configMAX_TASK_NAME_LEN			(12)
 #define configUSE_TRACE_FACILITY		1
